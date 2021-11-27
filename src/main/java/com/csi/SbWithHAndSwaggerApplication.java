2d8b668a1a9381b98da6c35ea311e81e34f58da4
Shubham <khuleshubham2504@gmail.com>
2021-11-27 08:46:25 +0530

    hiiii

---
+++ src/main/java/com/csi/SbWithHAndSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.csi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbWithHAndSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbWithHAndSwaggerApplication.class, args);
+	}
+
+}
